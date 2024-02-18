7e0eff40601ae999d509676f21c596b70ec80793
markcosmars2000 <32183341-markcosmars2000@users.noreply.replit.com>
2024-02-18 15:19:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+print("Hello world!\nHello world!\nHello world!")
+
+# indentation error
+#   print("Hello" + " " + "Angela"")
+
+# syntax errors
+print("Hello" + " " + "Angela")
+# undefined name.
+
+#Write your code below this line 👇
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+
+
+
+
+
+
+
+
