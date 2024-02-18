077852560001da0d55094b343e76ee1fe8803ea3
markcosmars2000 <32183341-markcosmars2000@users.noreply.replit.com>
2024-02-18 16:20:16 +0000

    named exercises and added exercise 1.3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # print("Day 1 - Python Print Function")
 # print("The function is declared like this:")
 # print("print('what to print')")
-
+# exer 1.1
 print("Hello world!\nHello world!\nHello world!")
 
 # indentation error
@@ -11,14 +11,19 @@ print("Hello world!\nHello world!\nHello world!")
 print("Hello" + " " + "Angela")
 # undefined name.
 
+# exercise 1.2
 #Write your code below this line 👇
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 
 
+# exercise 1.3 length of characters
+name = input("what is your name?")
+print(len(name))
 
-
+#shortest way
+print(len(input("what is your name?")))
 
 
 
